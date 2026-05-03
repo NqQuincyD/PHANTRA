580b44ba02f883989bfc3f648cff288e308d0f9f
NqQuincyD <nqopzendube@gmail.com>
2026-05-03 19:35:19 +0200

    Fixed missing datetime import in auth views

--- views/auth.py
+++ views/auth.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, render_template, redirect, url_for, flash, request
 from flask_login import login_user, logout_user, current_user
 from werkzeug.security import generate_password_hash
+from datetime import datetime
 
 from forms import RegistrationForm, LoginForm
 from models import db, User, AuthLog
